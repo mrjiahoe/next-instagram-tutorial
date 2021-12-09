8fe9e5aa1f3bd843e07a16249e82e7f3ba728fe4
jiahoe <mrjiahoe@hotmail.com>
2021-12-09 15:45:31 +0800

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,21 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
+		<div className="">
 			<Head>
 				<title>Instagram 2.0</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<h1>This is the INSTAGRAM 2.0 build</h1>
+			{/* Header */}
+			<Header />
+
+			{/* Feed */}
+
+			{/* Modal */}
 		</div>
 	);
 }
