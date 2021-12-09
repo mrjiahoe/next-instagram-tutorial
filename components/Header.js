4bb408352e96d54561e99da77c43b3e7f09c5332
jiahoe <mrjiahoe@hotmail.com>
2021-12-09 17:50:47 +0800

    added logo

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,28 @@ import Image from "next/image";
 const Header = () => {
 	return (
 		<div>
-			<h1>I'm the header</h1>
+			<div className="flex justify-between max-w-6xl">
+				{/* Left */}
+				<div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+					<Image
+						src="https://links.papareact.com/ocw"
+						layout="fill"
+						objectFit="contain"
+					/>
+				</div>
 
-			{/* Left */}
-			<div>
-				<Image src="https://links.papareact.com/ocw" layout="fill" />
+				<div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+					<Image
+						src="https://links.papareact.com/jjm"
+						layout="fill"
+						objectFit="contain"
+					/>
+				</div>
+
+				{/* Middle */}
+
+				{/* Right */}
 			</div>
-
-			{/* Middle */}
-
-			{/* Right */}
 		</div>
 	);
 };
