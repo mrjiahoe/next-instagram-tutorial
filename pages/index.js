bce012c704673dd4a68bf94660d3c28b7f260176
jiahoe <mrjiahoe@hotmail.com>
2021-12-09 23:35:19 +0800

    add feed

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,9 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			{/* Header */}
 			<Header />
 
-			{/* Feed */}
+			<Feed />
 
 			{/* Modal */}
 		</div>
