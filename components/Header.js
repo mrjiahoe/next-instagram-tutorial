8c2c40e9c617f8c64ab8879674ca31b136bee782
jiahoe <mrjiahoe@hotmail.com>
2021-12-09 18:42:54 +0800

    add search field

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import {
+	SearchIcon,
+	PlusCircleIcon,
+	UserGroupIcon,
+	HeartIcon,
+	PaperAirplaneIcon,
+	MenuIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
 	return (
 		<div>
 			<div className="flex justify-between max-w-6xl">
 				{/* Left */}
-				<div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+				<div className="relative hidden lg:inline-grid  w-24 cursor-pointer">
 					<Image
 						src="https://links.papareact.com/ocw"
 						layout="fill"
@@ -13,7 +21,7 @@ const Header = () => {
 					/>
 				</div>
 
-				<div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+				<div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
 					<Image
 						src="https://links.papareact.com/jjm"
 						layout="fill"
@@ -21,7 +29,17 @@ const Header = () => {
 					/>
 				</div>
 
-				{/* Middle */}
+				{/* Middle - Search input field */}
+				<div className="relative mt-1 p-3 rounded-md ">
+					<div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+						<SearchIcon className="h-5 w-5 text-gray-500" />
+					</div>
+					<input
+						className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+						type="text"
+						placeholder="Search"
+					/>
+				</div>
 
 				{/* Right */}
 			</div>
