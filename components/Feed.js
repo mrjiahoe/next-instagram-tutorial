e4390c0898744d97128d04af16ebe7bc002fd9a5
jiahoe <mrjiahoe@hotmail.com>
2021-12-10 16:27:08 +0800

    added insta stories

--- components/Feed.js
+++ components/Feed.js
@@ -1,14 +1,20 @@
+import Stories from "./Stories";
+
 function Feed() {
 	return (
-		<div>
-			{/* Section */}
-			    {/* Stories */}
-			    {/* Posts */}
+		<main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
+			<section className="col-span-2">
+				{/* Stories */}
+				<Stories />
+				{/* Posts */}
+			</section>
 
 			{/* Section */}
-			    {/* Mini Profile */}
-			    {/* Suggestions */}
-		</div>
+			<section>
+				{/* Mini Profile */}
+				{/* Suggestions */}
+			</section>
+		</main>
 	);
 }
 
