9ccf9ad6f60d0f1d4912b154ae0725d7086f2b76
jiahoe <mrjiahoe@hotmail.com>
2021-12-10 21:26:36 +0800

    styling mini profile

--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -7,12 +7,12 @@ function MiniProfile() {
 				alt="profile"
 			/>
 
-			<div>
-				<h2>heeeey</h2>
-				<h3>Welcome to Instagram</h3>
+			<div className="flex-1 mx-4">
+				<h2 className="font-bold">heeeey</h2>
+				<h3 className="text-sm text-gray-400">Welcome to Instagram</h3>
 			</div>
 
-			<button>Sign Out</button>
+			<button className="text-blue-400 text-sm font-semibold">Sign Out</button>
 		</div>
 	);
 }
