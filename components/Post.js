3dc8291b3bb7f5c7242f74387e3576300616acd5
jiahoe <mrjiahoe@hotmail.com>
2021-12-12 21:31:06 +0800

    remove button, comment when signed out

--- components/Post.js
+++ components/Post.js
@@ -7,8 +7,11 @@ import {
 	PaperAirplaneIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import { useSession } from "next-auth/react";
 
 function Post({ id, username, userImg, img, caption }) {
+	const { data: session } = useSession();
+
 	return (
 		<div className="bg-white my-7 border rounded-sm">
 			{/* Header */}
@@ -26,15 +29,17 @@ function Post({ id, username, userImg, img, caption }) {
 			<img src={img} className="object-cover w-full" alt="" />
 
 			{/* Buttons */}
-			<div className="flex justify-between px-4 pt-4">
-				<div className="flex space-x-4">
-					<HeartIcon className="btn" />
-					<ChatIcon className="btn" />
-					<PaperAirplaneIcon className="btn" />
-				</div>
+			{session && (
+				<div className="flex justify-between px-4 pt-4">
+					<div className="flex space-x-4">
+						<HeartIcon className="btn" />
+						<ChatIcon className="btn" />
+						<PaperAirplaneIcon className="btn" />
+					</div>
 
-				<BookmarkIcon className="btn" />
-			</div>
+					<BookmarkIcon className="btn" />
+				</div>
+			)}
 
 			{/* caption */}
 			<p className="p-5 truncate">
@@ -45,15 +50,17 @@ function Post({ id, username, userImg, img, caption }) {
 			{/* comments */}
 
 			{/* input box */}
-			<form className="flex items-center p-4">
-				<EmojiHappyIcon className="h-7" />
-				<input
-					type="text"
-					placeholder="Add a comment..."
-					className="border-none flex-1 focus:ring-0 outline-none"
-				/>
-				<button className="font-semibold text-blue-400">Post</button>
-			</form>
+			{session && (
+				<form className="flex items-center p-4">
+					<EmojiHappyIcon className="h-7" />
+					<input
+						type="text"
+						placeholder="Add a comment..."
+						className="border-none flex-1 focus:ring-0 outline-none"
+					/>
+					<button className="font-semibold text-blue-400">Post</button>
+				</form>
+			)}
 		</div>
 	);
 }
